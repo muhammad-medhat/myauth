22c99996e63bd6e765177bc3ee1fa63a80b8080a
muhammad medhat <muhammad.medhat@gmail.com>
2024-09-24 22:20:38 +0300

    adding hello request

--- api/hello.js
+++ api/hello.js
@@ -6,11 +6,7 @@ const app = express();
 app.use(cors());
 
 // Create the Express route
-app.get("/api/bool", (req, res) => {
-  const res = Math.random();
-  const result = res >= 0.5; // Random true/false
-  res.json({ result: result });
-});
+
 module.exports = (req, res) => {
   res.status(200).json({ message: "Hello World" });
 };
