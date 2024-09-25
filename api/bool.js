f4fc75044d0c2a23bb28bf20fcd6a4f58586714c
muhammad medhat <muhammad.medhat@gmail.com>
2024-09-25 18:20:38 +0300

    enable cors for single route

--- api/bool.js
+++ api/bool.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 
 // Create the Express route
-app.get("/api/bool", (req, res) => {
-  const res = Math.random();
-  const result = res >= 0.5; // Random true/false
+app.get("/api/bool", cors(), (req, res) => {
+  const rnd = Math.random();
+  const result = rnd >= 0.5; // Random true/false
   res.json({ result: result });
 });
 
